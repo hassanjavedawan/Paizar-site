c35d8c59d71880c910d476fab0f491a1f95eb641
Hassan Javed <hassanjaved836@gmail.com>
2023-02-25 03:01:53 +0500

    add cards

--- src/components/elements/Section2.jsx
+++ src/components/elements/Section2.jsx
@@ -1,15 +1,119 @@
-import React from 'react'
+/** @format */
+
+import React, { useState } from "react";
+import { BsEye, BsHeartFill, BsStarFill, BsStarHalf } from "react-icons/bs";
+import Modal from "react-bootstrap/Modal";
+import bestSeller from "../../data/bestSeller.json";
 
 const Section2 = () => {
-  return (
-    <div className='container-fluid mt-5'>
-        <div className="container">
-            <div className="row">
-                <h2 className='title_1'><span >Best seller products</span></h2>
-            </div>
-        </div>
-    </div>
-  )
-}
+  const [select, setSelect] = useState();
 
-export default Section2
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div className='container-fluid '>
+      <section className='categori'>
+        <h2 className='title_1'>
+          <span>Best seller products</span>
+        </h2>
+        <div className='custom-container-2 mt-5'>
+          <div className='categori-item '>
+            <div className='row g-3'>
+              {bestSeller.map((data, id) => {
+                return (
+                  <div className='col-lg-3 col-md-3 col-6' key={id}>
+                    <div className='categori-card card1'>
+                      <img className='w-100' src={data.imgUrl} alt='' />
+                      <div className='go-corner' href='#'>
+                        <div className='go-arrow'>
+                          <BsHeartFill />
+                        </div>
+                      </div>
+                      <div className='categori-card-body'>
+                        <div className='wrappers'>
+                          <div
+                            className='btns m-auto'
+                            onClick={() => {
+                              setSelect(data);
+                              handleShow();
+                            }}>
+                            <div className='side default-side'>Quick View</div>
+                            <div className='side hover-side'>
+                              <BsEye size={22} />
+                            </div>
+                          </div>
+                        </div>
+
+                        <br />
+                        <br />
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
+      <Modal show={show} centered onHide={handleClose}>
+        <Modal.Body>
+          <div className='row cardd'>
+            <div className='col-sm-6 p-0'>
+              <div className='box1'>
+                <img
+                  src={select?.imgUrl}
+                  className='m-auto d-block'
+                  width='85%'
+                  alt=''
+                />
+                <p className='textOne'>
+                  Available <span className='textTwo'>Sizes</span>
+                </p>
+                <ul className='menu list-inline'>
+                  <li>{select?.size}</li>
+
+                  <div className='badboy' />
+                </ul>
+                <p className='textThere'>Full Detail</p>
+              </div>
+            </div>
+            <div className='col-sm-6 p-0'>
+              <div className='box2'>
+                <div className='text-center'>
+                  <i className='fas fa-balance-scale' />
+                  <p className='textFour'>{select?.title}</p>
+
+                  <BsStarFill color='yellow' />
+                  <BsStarFill color='yellow' />
+                  <BsStarFill color='yellow' />
+                  <BsStarFill color='yellow' />
+                  <BsStarHalf color='yellow' />
+                </div>
+                <p className='bigText'>{select?.des}</p>
+                <div className='text-center'>
+                  <p className='number2'>
+                    Rs: {select?.pricePKR}{" "}
+                    <span className='number3'>{select?.priceDollar}$</span>
+                  </p>
+
+                  <a
+                    rel='noreferrer'
+                    href='https://api.whatsapp.com/send/?phone=923459392512'
+                    target='_blank'
+                    className='course-btn'>
+                    Place Order
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default Section2;
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -9,110 +9,114 @@ import { FaEnvelope, FaWhatsapp } from "react-icons/fa";
 // ======================°°°°°°°°°°°°°°°°°°°°°°°=========================
 const Header = () => {
   return (
-    // <header>
-    //   <nav className=' navbar navbar-expand-lg navigation-wrap'>
-    //     <div className='container-fluid'>
-    //       <div className='navbar-brand'>Free Home delivery </div>
+    <>
+      <nav className=' navbar navbar-expand-lg navigation-wrap'>
+        <div className='container-fluid'>
+          <div className='text-light para'>Free Home delivery </div>
+          <a
+            href='https://api.whatsapp.com/send/?phone=923459392512'
+            rel='noreferrer'
+            target='_blank'
+            className='pc-wa nav-link'>
+            <FaWhatsapp color='white' size={22} /> 0345-9392512
+          </a>
+          {/* <ul className='navbar-nav me-auto mb-2 mb-lg-0'></ul> */}
+          <span className='navbar-text'>
+            <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+              <li className='nav-item pl-4 pl-md-0 ml-0 ml-md-4'>
+                <a
+                  href='https://api.whatsapp.com/send/?phone=923459392512'
+                  rel='noreferrer'
+                  target='_blank'
+                  className='nav-link'>
+                  <FaWhatsapp color='white' size={22} /> 0345-9392512
+                </a>
+              </li>
 
-    //       <div className='collapse navbar-collapse' id='navbarText'>
-    //         <ul className='navbar-nav me-auto mb-2 mb-lg-0'></ul>
-    //         <span className='navbar-text'>
-    //           <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-    //             <li className='nav-item pl-4 pl-md-0 ml-0 ml-md-4'>
-    //               <a
-    //                 href='https://api.whatsapp.com/send/?phone=923459392512'
-    //                 rel='noreferrer'
-    //                 target='_blank'
-    //                 className='nav-link'>
-    //                 <FaWhatsapp color='green' size={22} /> 0345-9392512
-    //               </a>
-    //             </li>
+              <li className='nav-item pl-4 pl-md-0 ml-0 ml-md-4'>
+                <a href='mailto:Paizarhandmade@gmail.com' className='nav-link'>
+                  <span>
+                    <FaEnvelope color='#fff' size={22} />{" "}
+                    Paizarhandmade@gmail.com
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </span>
+        </div>
+      </nav>
 
-    //             <li className='nav-item pl-4 pl-md-0 ml-0 ml-md-4'>
-    //               <a
-    //                 href='mailto:Paizarhandmade@gmail.com'
-    //                 className='nav-link'>
-    //                 <FaEnvelope color='#000' size={22} />{" "}
-    //                 Paizarhandmade@gmail.com
-    //               </a>
-    //             </li>
-    //           </ul>
-    //         </span>
-    //       </div>
-    //     </div>
-    //   </nav>
-
-    // </header>
-    <header>
-      <div className='custom-container-1'>
-        <nav className='navbar navbar-expand-lg  our-nav'>
-          <div className='container-fluid'>
-            <div className='header-logo'>
-              <a className='navbar-brand' href='#'>
-                <div className='header-logo-img'>Paizar</div>
-              </a>
-              <div className='header-input'>
-                <input type='search' placeholder='Search courses..' />
-                <span>
-                  <i className='fa-regular fa-magnifying-glass' />
-                </span>
+      <header>
+        <div className='custom-container-1'>
+          <nav className='navbar navbar-expand-lg  our-nav'>
+            <div className='container-fluid'>
+              <div className='header-logo'>
+                <a className='navbar-brand' href='#'>
+                  <div className='header-logo-img'>Paizar</div>
+                </a>
+                {/* <div className='header-input'>
+                  <input type='search' placeholder='Search courses..' />
+                  <span>
+                    <i className='fa-regular fa-magnifying-glass' />
+                  </span>
+                </div> */}
               </div>
-            </div>
-            <button
-              className='navbar-toggler'
-              type='button'
-              data-bs-toggle='collapse'
-              data-bs-target='#navbarNav'
-              aria-controls='navbarNav'
-              aria-expanded='false'
-              aria-label='Toggle navigation'>
-              <span className='navbar-toggler-icon' />
-            </button>
-            <div className='collapse navbar-collapse' id='navbarNav'>
-              <ul className='navbar-nav menu'>
-                <li>
-                  <a className='menu-first-a' href='../Home/index.html'>
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a className='menu-first-a' href='#'>
-                    Browse Courses
-                  </a>
-                </li>
-                <li>
-                  <a className='menu-first-a' href='#'>
-                    About
-                  </a>
-                </li>
-                <li>
-                  <a className='menu-first-a' href='#'>
-                    Blog
-                  </a>
-                  <ul className='dropdown'>
-                    <li>
-                      <a className='dropdown-a' href='#'>
-                        Blog
-                      </a>
-                    </li>
-                    <li>
-                      <a className='dropdown-a' href='#'>
-                        Blog Details
-                      </a>
-                    </li>
-                    <li>
-                      <a className='dropdown-a' href='#'>
-                        Elements
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a className='menu-first-a' href='../Contuct/index.html'>
-                    Contuct
-                  </a>
-                </li>
-                {/* <li>
+
+              <button
+                className='navbar-toggler'
+                type='button'
+                data-bs-toggle='collapse'
+                data-bs-target='#navbarNav'
+                aria-controls='navbarNav'
+                aria-expanded='false'
+                aria-label='Toggle navigation'>
+                <span className='navbar-toggler-icon' />
+              </button>
+              <div className='collapse navbar-collapse' id='navbarNav'>
+                <ul className='navbar-nav menu'>
+                  <li>
+                    <a className='menu-first-a' href='../Home/index.html'>
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a className='menu-first-a' href='#'>
+                      Browse Courses
+                    </a>
+                  </li>
+                  <li>
+                    <a className='menu-first-a' href='#'>
+                      About
+                    </a>
+                  </li>
+                  <li>
+                    <a className='menu-first-a' href='#'>
+                      Blog
+                    </a>
+                    <ul className='dropdown'>
+                      <li>
+                        <a className='dropdown-a' href='#'>
+                          Blog
+                        </a>
+                      </li>
+                      <li>
+                        <a className='dropdown-a' href='#'>
+                          Blog Details
+                        </a>
+                      </li>
+                      <li>
+                        <a className='dropdown-a' href='#'>
+                          Elements
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a className='menu-first-a' href='../Contuct/index.html'>
+                      Contuct
+                    </a>
+                  </li>
+                  {/* <li>
                   <div className='menu-shopping'>
                     <a className='menu-shopping-card' href='#'>
                       <span>
@@ -122,18 +126,21 @@ const Header = () => {
                     <span>1</span>
                   </div>
                 </li> */}
-                <li>
-                  <a href='https://api.whatsapp.com/send/?phone=923459392512' target="_blank"  rel="noreferrer" >
-                  <FaWhatsapp size={30} color="green" />
-
-                  </a>
-                </li>
-              </ul>
+                  {/* <li>
+                    <a
+                      href='https://api.whatsapp.com/send/?phone=923459392512'
+                      target='_blank'
+                      rel='noreferrer'>
+                      <FaWhatsapp size={30} color='green' />
+                    </a>
+                  </li> */}
+                </ul>
+              </div>
             </div>
-          </div>
-        </nav>
-      </div>
-    </header>
+          </nav>
+        </div>
+      </header>
+    </>
   );
 };
 
